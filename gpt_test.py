2b19faa712bf95d94ace10f5a710bb4b787f9393
zhiyi <eririsepanda@gmail.com>
2024-12-20 13:13:28 +0700

    gpt test

---
+++ gpt_test.py
@@ -0,0 +1,15 @@
+from openai import OpenAI
+client = OpenAI(api_key="")
+
+completion = client.chat.completions.create(
+    model="gpt-4o",
+    messages=[
+        {"role": "developer", "content": "You are a helpful assistant."},
+        {
+            "role": "user",
+            "content": "Write a haiku about recursion in programming."
+        }
+    ]
+)
+
+print(completion.choices[0].message)

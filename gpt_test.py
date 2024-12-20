a7f9e92d325af55573526b87569689b6de4ec44e
zhiyi <eririsepanda@gmail.com>
2024-12-20 14:24:51 +0700

    updates

--- gpt_test.py
+++ gpt_test.py
@@ -1,5 +1,5 @@
 from openai import OpenAI
-client = OpenAI(api_key="")
+client = OpenAI()
 
 completion = client.chat.completions.create(
     model="gpt-4o",
--- zhiyi_test.py
+++ zhiyi_test.py
@@ -1,6 +1,7 @@
-from transformers import pipeline
+from transformers import pipeline, AutoTokenizer, AutoModelForSequenceClassification, AutoModelForCausalLM
 import streamlit as st
 import random
+import torch
 import time
 
 st.title("Mood Detection")
@@ -14,14 +15,25 @@ for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
 
+def get_highest_emotion(emotion_scores):
+    # Find the emotion with the maximum score
+    highest_emotion = max(emotion_scores, key=lambda x: x["score"])
+    return highest_emotion["label"], highest_emotion["score"]
+
 def run_bert_eval(prompt):
     # this bert encoder model is finetuned on bert-base-uncased emotion dataset to detect emotion
     classifier = pipeline("text-classification",
                         model='bhadresh-savani/bert-base-uncased-emotion', 
                         return_all_scores=True)
     prediction = classifier(prompt, )
-    return prediction
+    
+    return get_highest_emotion(prediction[0])[0]
 
+def decoder():
+    # DialoGPT model
+    dialogpt_tokenizer = AutoTokenizer.from_pretrained("microsoft/DialoGPT-medium")
+    dialogpt_model = AutoModelForCausalLM.from_pretrained("microsoft/DialoGPT-medium")
+    
 # Accept user input
 if prompt := st.chat_input("What is up?"):
     # Display user message in chat message container
@@ -32,6 +44,6 @@ if prompt := st.chat_input("What is up?"):
 
     with st.chat_message("assistant"):
         print("prompt: ", prompt)
-        response = st.write_stream(run_bert_eval(prompt))
+        response = st.write(run_bert_eval(prompt))
     # Add assistant response to chat history
     st.session_state.messages.append({"role": "assistant", "content": response})
